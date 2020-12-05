567c4b57055eaf8257163c2d70b9c285ebd8f8e2
cemutku <33598193+cemutku@users.noreply.github.com>
2020-12-05 13:13:36 +0300

    Update seed queries

--- backend/src/utils/queries.js
+++ backend/src/utils/queries.js
@@ -1,18 +1,19 @@
 const createShortUrlTable = `
 DROP TABLE IF EXISTS shortened_url;
-CREATE TABLE IF NOT EXISTS shortened_url (
-  short_url VARCHAR(8) NOT NULL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS shortened_url 
+(
+  short_url VARCHAR(8) NOT NULL,
   long_url VARCHAR(4000) NOT NULL,
-  created_at TIMESTAMP DEFAULT NOW()
-  )
-  `;
+  created_at TIMESTAMP DEFAULT NOW(),
+  CONSTRAINT shortened_url_pkey PRIMARY KEY (short_url)
+)`;
 
 const insertShortUrls = `
-INSERT INTO shortened_url(long_url, short_url)
-VALUES ('https://www.test-long-url/dawdawdawdamdawd', '12adf4rg')
+INSERT INTO shortened_url(short_url, long_url)
+VALUES ('12adf4rg', 'https://www.test-long-url/test')
 `;
 
-const dropShortUrlTable = 'DROP TABLE shortened_url';
+const dropShortUrlTable = 'DROP TABLE IF EXISTS shortened_url';
 
 module.exports = {
 	createShortUrlTable,
